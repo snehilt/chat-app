240a4f154fae9339e1a309c164a87cf772bfae1f
snehilt <tsnehil1998@gmail.com>
2024-01-07 22:45:54 +0530

    (feat): UI change for sign in and sign up page
    UI change for sign in and sign up page

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -6,8 +6,18 @@ type Props = {
 
 const AuthLayout = ({ children }: Props) => {
   return (
-    <div className="flex items-center justify-center h-full bg-red-600">
-      {children}
+    <div className="flex items-center flex-col gap-8 justify-center h-full bg-gradient-to-tl from-black to-[#232428} w-full relative ">
+      <img
+        src="/people.jpg"
+        className="w-full h-full object-cover absolute mix-blend-overlay"
+      />
+      <div className="flex flex-col items-center gap-2">
+        <h1 className="text-7xl text-gray-300 font-semibold">Convo.</h1>
+        <p className="text-lg font-semibold text-zinc-500">
+          Get in touch with everyone.
+        </p>
+      </div>
+      <div>{children}</div>
     </div>
   );
 };
